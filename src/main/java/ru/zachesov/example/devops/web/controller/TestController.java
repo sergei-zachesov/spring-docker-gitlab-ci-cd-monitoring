a4af74cbd13cf8680212d386232956871d88d758
Sergei <szachesov@yandex.ru>
2025-01-14 15:02:26 +0300

    add spring profile

--- src/main/java/ru/zachesov/example/devops/web/controller/TestController.java
+++ src/main/java/ru/zachesov/example/devops/web/controller/TestController.java
@@ -1,6 +1,7 @@
 package ru.zachesov.example.devops.web.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 @RestController
 public class TestController {
-    @GetMapping("/hello")
-    public ResponseEntity<String> getTest() {
 
-        log.info("hello");
-        return ResponseEntity.ok("Hello world!!!!!!!!!!!!!!!!!!!!!!");
-    }
+  @Value("${test}")
+  private String test;
+
+  @GetMapping("/hello")
+  public ResponseEntity<String> getTest() {
+    log.info("hello");
+    return ResponseEntity.ok("Hello world, test!!!!!!!!!!!!!!!!!!!!!!");
+  }
 }
